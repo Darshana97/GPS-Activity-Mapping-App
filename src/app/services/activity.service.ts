ed846934f7eb955bf1baa0e272958694874db14e
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-05-09 15:42:53 +0530

    activities

--- src/app/services/activity.service.ts
+++ src/app/services/activity.service.ts
@@ -11,4 +11,27 @@ export class ActivityService {
   getActivities(): IActivity[] {
     return SAVED_ACTIVITIES.slice(0);
   }
+
+  getTotalActivities(allActivities: IActivity[]) {
+    return allActivities.length;
+  }
+
+  getTotalDistance(allActivities: IActivity[]) {
+    var totalDistance = 0;
+    for (var i = 0; i < allActivities.length; i++) {
+      totalDistance += allActivities[i].distance;
+    }
+    return totalDistance;
+  }
+
+  getFirstDate(allActivities: IActivity[]) {
+    var earliestDate = new Date("01/01/9999");
+    for (var i = 0; i < allActivities.length; i++) {
+      var currentDate = allActivities[i].date;
+      if (currentDate < earliestDate) {
+        earliestDate = currentDate;
+      }
+    }
+    return earliestDate;
+  }
 }
