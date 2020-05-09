8701d88ab177e3ba0e185caaf9d44e3a0b576a28
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-05-09 21:01:46 +0530

    import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ActivityListComponent } from './activity-list/activity-list.component';
-import { MapComponent } from './map/map.component';
-import { ActivityService } from './services/activity.service';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ActivityListComponent } from "./activity-list/activity-list.component";
+import { MapComponent } from "./map/map.component";
+import { ActivityService } from "./services/activity.service";
+import { appRoute } from "../routes";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ActivityListComponent,
-    MapComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ActivityListComponent, MapComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [ActivityService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
