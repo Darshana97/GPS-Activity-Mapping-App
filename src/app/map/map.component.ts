a5932504d698c0b14f4b9e9c90b0514de5137617
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-05-10 11:35:59 +0530

    import modules for map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from "@angular/core";
+import { MapService } from "../services/map.service";
+import { IActivity } from "../shared/activity.model";
+import { ActivatedRoute } from "@angular/router";
+
 
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  selector: "app-map",
+  templateUrl: "./map.component.html",
+  styleUrls: ["./map.component.css"],
 })
 export class MapComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
