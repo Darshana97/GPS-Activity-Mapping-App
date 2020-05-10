1391a50df8bba4ceefff5250438788f8d38b5b83
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-05-10 11:45:58 +0530

    map component design

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -3,14 +3,27 @@ import { MapService } from "../services/map.service";
 import { IActivity } from "../shared/activity.model";
 import { ActivatedRoute } from "@angular/router";
 
-
 @Component({
   selector: "app-map",
   templateUrl: "./map.component.html",
   styleUrls: ["./map.component.css"],
 })
 export class MapComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private _mapServise: MapService,
+    private _route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  activity: any;
+  activityName: string;
+  activityComments: string;
+  activityDate: Date;
+  activityDistance: number;
+  gpx: any;
+
+  ngOnInit(): void {
+    this.activity = this._mapServise.getActivity(
+      +this._route.snapshot.params["id"]
+    );
+  }
 }
